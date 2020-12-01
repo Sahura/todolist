093b2f6e056c3be1bb45b6ebf1f1c52d13a3c911
Sahura <1747889818@qq.com>
2020-12-01 20:51:19 +0800

    ui基本完成

--- todolist/src/App.vue
+++ todolist/src/App.vue
@@ -1,112 +1,145 @@
 <template>
-  <v-app id="app"> 
-    <h1 >ToDo
-      <input style="place:right;background-color:white" type="text" v-model="newTask" placeholder="blablabla">
-      <button @click="addNewTask">Add</button>
-    </h1>
-    
-    
-    <v-card
-    
-    class="done"
-    v-for="(task,index) in list" 
-    :key="index"
-    
-    v-show="!task.done"
-    >
-     <button @click=" done(index)">change  </button> {{task.task}}    <button @click="remove(index)">Remove</button>
+  <v-app id="app">
+    <h1 style="display: flex; flex-wrap: wrap; justify-content: space-between">
+      <div>ToDo</div>
 
-    </v-card>
+      <div style="bakgroud-color: blue">
+        <input
+          style="background-color: #888888; margin-right: 10px"
+          type="text"
+          v-model="newTask"
+          placeholder="blablabla"
+        />
+        <button @click="addNewTask">ADD</button>
+
+        <!-- <v-btn
+        color="white"
+        outlined
+        @click="addNewTask">Add</v-btn> -->
+      </div>
+    </h1>
+    <div class="placeCard">
+      <v-card
+        max-width="600px"
+        class="cards"
+        v-for="(task, index) in list"
+        :key="index"
+        v-show="!task.done"
+      >
+        <v-checkbox v-model="task.done"></v-checkbox>
+        <!-- <button @click="done(index)">change</button> -->
+        <div class="undoneTask">
+          {{ task.task }}
+        </div>
+        <v-icon @click="remove(index)">mdi-cancel</v-icon>
+      </v-card>
+    </div>
 
     <h1>Done</h1>
-    <v-card
-    class="unDone" 
-    v-for="(task,index) in list" 
-    :key="index"
-   
-    v-show="task.done"
-    >
-    <button @click="unDone(index)"> change  </button>  {{task.task}}    <button @click="remove(index)" >Remove</button>  
+    <div class="placeCard">
+      <v-card
+        max-width="600px"
+        class="cards"
+        v-for="(task, index) in list"
+        :key="index"
+        v-show="task.done"
+      >
+        <v-checkbox v-model="task.done"></v-checkbox>
+        <!-- <button @click="unDone(index)">change</button> -->
+        <div class="doneTask">
+          {{ task.task }}
+        </div>
 
-    </v-card>
-    
-     
+        <v-icon @click="remove(index)">mdi-cancel</v-icon>
+      </v-card>
+    </div>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
     HelloWorld,
-    
   },
-  data(){
-    return{
-      title:"down",
-      list:[
+  data() {
+    return {
+      list: [
         {
-          done:false,
-          task:"aaaa"        
+          done: false,
+          task: "aaaa",
         },
-        
-        {
-          done:true,
-          task:"bbbb"        
-        },
-        
-        {
-          done:false,
-          task:"cccc"        
-        },
-        
-      ]
-    }
-  },
-  methods:{
-    updateTitle(e){
-      this.title = e ;
-    },
-    done(index){
-      return this.list[index].done=true
-    },
-    unDone(index){
-      return this.list[index].done=false
-    },
-    addNewTask(){
-      this.list.unshift({
-        done:false,
-        task:this.newTask
-      }),
-      this.newTask=""
-    },
-    remove(index){
-      this.list.splice(index,1)
-     
-    }
-  }
- 
-};
 
+        {
+          done: true,
+          task: "bbbb",
+        },
+
+        {
+          done: false,
+          task: "cccc",
+        },
+      ],
+    };
+  },
+  methods: {
+    updateTitle(e) {
+      this.title = e;
+    },
+    done(index) {
+      return (this.list[index].done = true);
+    },
+    unDone(index) {
+      return (this.list[index].done = false);
+    },
+    addNewTask() {
+      this.list.unshift({
+        done: false,
+        task: this.newTask,
+      }),
+        (this.newTask = "");
+    },
+    remove(index) {
+      this.list.splice(index, 1);
+    },
+  },
+};
 </script>
 
 <style scoped>
-  #app{
-    margin:auto;
-  }
-  .done{
-    color:green
-  }
-  .unDone{
-    color:grey;
-    
-    border-width:10px;
-  }
-  h1{
-    background-color:black ;
-    color:white
-  }
+#app {
+  margin: auto;
+}
+
+.cards {
+  padding: 0 10px;
+  margin: 5px auto;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+h1 {
+  padding: 10px;
+  background-color: black;
+  color: white;
+}
+.doneTask {
+  font-size: 30px;
+  color: grey;
+  text-decoration: line-through;
+  width: 70%;
+  margin: 10px;
+}
+.undoneTask {
+  font-size: 30px;
+  color: black;
+  width: 70%;
+  margin: 10px;
+}
+.placeCard {
+  margin: 10px;
+}
 </style>
